e609cc24aef95db0f554cd882ee44c4cc3d70ebc
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2022-03-26 18:00:24 -0300

    resolve requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not isinstance(word, str) or word == "":
+        return False
+
+    if low_index > high_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    return False
+
+
+# print(is_palindrome_recursive("PAULA", 0, 4))
+# print(is_palindrome_recursive("ANA", 0, 2))
+# print(is_palindrome_recursive("SOCOS", 0, 4))
+# print(is_palindrome_recursive("COXINHA", 0, 6))
